bda77cae343af826df18b95b002d98e69840261f
kofoworola <adenekankofoworola@gmail.com>
2023-01-06 10:35:26 +0100

    implement login page and modify existing components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import  GlobalStyle  from './globalStyles.styled'
-import Navbar from './components/Navbar'
-import Banner from 'components/Banner'
+import { Routes, Route } from "react-router-dom"
+import Home from 'Pages/Home';
+import Login from 'Pages/Login';
+
 
 const App:React.FC = ()=> (
   <>
-    <GlobalStyle />
-    <Navbar />
-    <Banner />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+    </Routes>
   </>
 )
 
---
+++ src/components/InputFields/inputfields.styled.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const InputWrapper = styled.input`
+    width: 90%;
+    border-style: none;
+    border-radius: 10px;
+    padding: 15px 10px;
+
+
+    &:focus {
+        outline: none;
+    }
+`
